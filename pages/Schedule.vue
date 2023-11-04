3f4182afa6b73fda3bfde597967f2db2a9736f45
Jesse Stewart <jesse.stewart@nlsnow.com>
2023-11-04 01:23:39 -0500

    Setting up layout

--- pages/Schedule.vue
+++ pages/Schedule.vue
@@ -1,10 +1,194 @@
 // https://tailwindui.com/components/application-ui/lists/feeds
-<script lang="ts" setup></script>
-
 <template>
-  <div>
-    Schedule
+  <div class="min-h-full">
+    <Disclosure as="nav" class="border-b border-gray-200 bg-white" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 justify-between">
+          <div class="flex">
+            <div class="flex flex-shrink-0 items-center">
+              <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+              <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+            </div>
+            <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            </div>
+          </div>
+          <div class="hidden sm:ml-6 sm:flex sm:items-center">
+            <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <!-- Profile dropdown -->
+            <Menu as="div" class="relative ml-3">
+              <div>
+                <MenuButton class="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  <span class="absolute -inset-1.5" />
+                  <span class="sr-only">Open user menu</span>
+                  <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                </MenuButton>
+              </div>
+              <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                    <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+          <div class="-mr-2 flex items-center sm:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="absolute -inset-0.5" />
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel class="sm:hidden">
+        <div class="space-y-1 pb-3 pt-2">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'border-indigo-500 bg-indigo-50 text-indigo-700' : 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800', 'block border-l-4 py-2 pl-3 pr-4 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        </div>
+        <div class="border-t border-gray-200 pb-3 pt-4">
+          <div class="flex items-center px-4">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium text-gray-800">{{ user.name }}</div>
+              <div class="text-sm font-medium text-gray-500">{{ user.email }}</div>
+            </div>
+            <button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-3 space-y-1">
+            <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">{{ item.name }}</DisclosureButton>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+
+    <div class="py-10">
+      <header>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Schedule</h1>
+        </div>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <!-- Your content -->
+          <div class="flow-root">
+            <ul role="list" class="-mb-8">
+              <li v-for="(event, eventIdx) in timeline" :key="event.id">
+                <div class="relative pb-8">
+                  <span v-if="eventIdx !== timeline.length - 1" class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                  <div class="relative flex space-x-3">
+                    <div>
+                      <span :class="[event.iconBackground, 'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white']">
+                        <component :is="event.icon" class="h-5 w-5 text-white" aria-hidden="true" />
+                      </span>
+                    </div>
+                    <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                      <div>
+                        <p class="text-sm text-gray-500">
+                          {{ event.content }} <a :href="event.href" class="font-medium text-gray-900">{{ event.target }}</a>
+                        </p>
+                      </div>
+                      <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                        <time :datetime="event.datetime">{{ event.date }}</time>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { CheckIcon, HandThumbUpIcon, UserIcon } from '@heroicons/vue/20/solid'
+
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Alerts', href: '/alerts', current: false },
+  { name: 'Help', href: '/help', current: false },
+  { name: 'Profile', href: '/profile', current: false },
+  { name: 'Schedule', href: '/schedule', current: true },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '/profile' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+const timeline = [
+  {
+    id: 1,
+    content: 'Applied to',
+    target: 'Front End Developer',
+    href: '#',
+    date: 'Sep 20',
+    datetime: '2020-09-20',
+    icon: UserIcon,
+    iconBackground: 'bg-gray-400',
+  },
+  {
+    id: 2,
+    content: 'Advanced to phone screening by',
+    target: 'Bethany Blake',
+    href: '#',
+    date: 'Sep 22',
+    datetime: '2020-09-22',
+    icon: HandThumbUpIcon,
+    iconBackground: 'bg-blue-500',
+  },
+  {
+    id: 3,
+    content: 'Completed phone screening with',
+    target: 'Martha Gardner',
+    href: '#',
+    date: 'Sep 28',
+    datetime: '2020-09-28',
+    icon: CheckIcon,
+    iconBackground: 'bg-green-500',
+  },
+  {
+    id: 4,
+    content: 'Advanced to interview by',
+    target: 'Bethany Blake',
+    href: '#',
+    date: 'Sep 30',
+    datetime: '2020-09-30',
+    icon: HandThumbUpIcon,
+    iconBackground: 'bg-blue-500',
+  },
+  {
+    id: 5,
+    content: 'Completed interview with',
+    target: 'Katherine Snyder',
+    href: '#',
+    date: 'Oct 4',
+    datetime: '2020-10-04',
+    icon: CheckIcon,
+    iconBackground: 'bg-green-500',
+  },
+]
+</script>
