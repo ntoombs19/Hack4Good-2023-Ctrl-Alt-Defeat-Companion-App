46f309af3a3d5caebfd7cc2d7a8e3b44cc14e7f7
Jesse Stewart <jesse.stewart@nlsnow.com>
2023-11-04 17:21:30 -0500

    Saving progress

--- components/ProfileForm.vue
+++ components/ProfileForm.vue
@@ -1,14 +1,13 @@
 <template>
-  {{ profile }}
   <form>
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input :value="profile.clientName" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Jane Smith" />
+                <input v-model="profile.clientName" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Jane Smith" />
               </div>
             </div>
           </div>
@@ -16,7 +15,7 @@
           <div class="sm:col-span-4">
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Date</label>
             <div class="mt-2">
-              <input class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" type="date" id="start" name="trip-start" :value="profile.date" min="2023-01-01" max="2025-12-31" />
+              <input class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" type="date" id="start" name="trip-start" v-model="profile.date" min="2023-01-01" max="2025-12-31" />
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
               </div>
             </div>
@@ -26,28 +25,28 @@
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Address</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input :value="profile.addressOne" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Address Line 1" />
+                <input v-model="profile.addressOne" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Address Line 1" />
               </div>
             </div>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input :value="profile.addressTwo" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Address Line 2" />
+                <input v-model="profile.addressTwo" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Address Line 2" />
               </div>
             </div>
             <div class="mt-2">
               <div class="flex">
                 <span class="mr-1 grow rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input :value="profile.city" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="City" />
+                  <input v-model="profile.city" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="City" />
                 </span>
                 <span class="mr-1 grow rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input :value="profile.state" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="State" />
+                  <input v-model="profile.state" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="State" />
                 </span>
               </div>
             </div>
             <div class="mt-2">
               <div class="flex">
                 <span class="mr-1 grow rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input :value="profile.zip" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Zip" />
+                  <input v-model="profile.zip" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Zip" />
                 </span>
               </div>
             </div>
@@ -57,7 +56,7 @@
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Employer</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input :value="profile.employer" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Jane Smith" />
+                <input v-model="profile.employer" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Jane Smith" />
               </div>
             </div>
           </div>
@@ -73,7 +72,7 @@
                   <option>EU</option>
                 </select>
               </div>
-              <input type="text" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
+              <input v-model="profile.mainPhone" type="text" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
             </div>
           </div>
 
@@ -88,9 +87,77 @@
                   <option>EU</option>
                 </select>
               </div>
-              <input :value="workPhone" type="text" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
+              <input v-model="profile.workPhone" type="text" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
             </div>
           </div>
+
+          <div>
+            <label for="phone-number" class="block text-sm font-medium leading-6 text-gray-900">Other Phone</label>
+            <div class="relative mt-2 rounded-md shadow-sm">
+              <div class="absolute inset-y-0 left-0 flex items-center">
+                <label for="country" class="sr-only">Country</label>
+                <select id="country" name="country" autocomplete="country" class="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                  <option>US</option>
+                  <option>CA</option>
+                  <option>EU</option>
+                </select>
+              </div>
+              <input v-model="profile.otherPhone" type="text" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
+            </div>
+          </div>
+
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+            <div class="mt-2">
+              <input v-model="profile.email" type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="you@example.com" />
+            </div>
+          </div>
+
+          <div class="w-full rounded overflow-hidden shadow-lg">
+            <div class="border-gray-200 bg-white px-4 py-5 sm:px-6">
+              <h3 class="text-lg font-semibold leading-6 text-gray-900">Interested Party</h3>
+              <div>
+                <div class="mt-3 mb-1 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input v-model="profile.interestedParty.name" type="text" name="clientName" id="clientName" autocomplete="clientName" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Name" />
+                </div>
+                <div class="relative mt-2 rounded-md shadow-sm">
+                <div class="absolute inset-y-0 left-0 flex items-center">
+                  <label for="country" class="sr-only">Country</label>
+                  <select id="country" name="country" autocomplete="country" class="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                    <option>US</option>
+                    <option>CA</option>
+                    <option>EU</option>
+                  </select>
+                </div>
+                <input v-model="profile.interestedParty.phone" type="text" name="phone-number" id="phone-number" class="mb-2 block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="+1 (555) 987-6543" />
+                </div>
+                <label for="location" class="block text-sm font-medium leading-6 text-gray-900">Role</label>
+                <select id="location" name="location" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <option v-for="role in profile.interestedParty.role">{{role}}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div v-for="child in profile.children" class="w-full rounded overflow-hidden shadow-lg">
+            <div class="border-gray-200 bg-white px-4 py-5 sm:px-6">
+              <h3 class="text-lg font-semibold leading-6 text-gray-900">Children</h3>
+              <div>
+                <div class="mt-3 mb-1 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input v-model="child.name" type="text" name="clientName" id="clientName" autocomplete="clientName" class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Name" />
+                  <input v-model="child.age" type="number" id="points" name="points" step="1" class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                </div>
+                <div class="relative mt-2 rounded-md shadow-sm">
+                </div>
+                <label for="location" class="block text-sm font-medium leading-6 text-gray-900">Role</label>
+                <select id="location" name="location" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <option v-for="contact in child.contactStatus">{{contact}}</option>
+                </select>
+              </div>
+            </div>
+          </div>          
+
+          {{ profile.children }}
 
           <div>
             <label for="price" class="block text-sm font-medium leading-6 text-gray-900">How much do you pay in child support per month?</label>
@@ -98,7 +165,7 @@
               <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <span class="text-gray-500 sm:text-sm">$</span>
               </div>
-              <input type="text" :value="profile.childSupportPerMonth" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" aria-describedby="price-currency" />
+              <input type="text" v-model="profile.childSupportPerMonth" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" aria-describedby="price-currency" />
               <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                 <span class="text-gray-500 sm:text-sm" id="price-currency">USD</span>
               </div>
@@ -259,6 +326,14 @@
 
 <script setup>
 import { UserCircleIcon } from '@heroicons/vue/24/solid'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const incrementCount = () => {
+  count.value++;
+  console.log(count.value)
+}
 
 const props = defineProps(['profile'])
 
--- pages/Profile.vue
+++ pages/Profile.vue
@@ -20,7 +20,7 @@ const currentPage = {
   name: "Alerts"
 }
 
-const profile = {
+const profile = ref({
   clientName: 'Tom Cook',
   date: '2018-07-22',
   addressOne: '123 Street Rd',
@@ -29,29 +29,29 @@ const profile = {
   state: 'MO',
   zip: '65807',
   employer: 'Good Dads',
-  mainPhone: '(417) 860-1111',
-  workPhone: '(417) 860-2222',
-  otherNumber: '',
-  email: '',
+  mainPhone: '(417) 123-1111',
+  workPhone: '(417) 123-2222',
+  otherNumber: '(417) 123-3333',
+  email: 'your@email.com',
   interestedParty: {
-    name: '',
-    phone: '',
-    role: {
-      probationOfficer: '',
-      paroleOffice: '',
-      caseWorker: ''
-    }
+    name: 'John',
+    phone: '(417) 123-3333',
+    role: [
+      'Probation Officer',
+      'Parole Officer',
+      'Case Worker'
+    ]
   },
   children: [{
-    name: '',
-    age: '',
-    dob: '',
-    contact: {
-      custody: false,
-      visitation: false,
-      phone: true,
-      none: false,
-    }
+    name: 'Timmy',
+    age: '4',
+    dob: '10/1/2019',
+    contactStatus: [
+      'custody',
+      'visitation',
+      'phone',
+      'none',
+    ]
   }],
   childSupportPerMonth: 200,
   maritalStatus: {
@@ -69,7 +69,7 @@ const profile = {
     white: false,
   },
   imageUrl: '',
-}
+})
 const navigation = [
   { name: 'Alerts', href: '/alerts', current: false, icon: icons.alert },
   { name: 'Help', href: '/help', current: false, icon: icons.help },
