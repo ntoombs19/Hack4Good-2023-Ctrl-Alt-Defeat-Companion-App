78956d1047838e37d8ffb855c86f88893bb19273
Carmen Creswell <carmencreswell@gmail.com>
2023-11-05 01:09:45 -0500

    Add images and brand colors

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -1,12 +1,11 @@
-
 <template>
-  <div class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+  <div class="fixed bottom-0 left-0 z-50 w-full h-16 border-t border-gray-200 bg-dark-green">
     <div class="grid h-full max-w-sm grid-cols-5 mx-auto font-medium">
-      <div v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
+      <div v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-500 text-white' : 'border-transparent text-white hover:border-gray-300 hover:text-gray-700', 'inline-flex items-center border-b-2 pt-1 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
         <NuxtLink :to="item.href">
-          <button type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-            <component class="justify-center w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" :is="item.icon"></component>
-            <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{{item.name}}</span>
+          <button type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-dark-green-hover">
+            <component class="justify-center w-5 h-5 mb-2 text-white group-hover:text-light-green" :is="item.icon"></component>
+            <span class="text-sm text-white group-hover:text-light-green">{{item.name}}</span>
           </button>
         </NuxtLink>
       </div>
