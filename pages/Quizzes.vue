b010dee487616c002df6476fa60ae80b1ae213d1
Jesse Stewart <jesse.stewart@nlsnow.com>
2023-11-05 00:10:36 -0500

    Progress..

--- pages/Quizzes.vue
+++ pages/Quizzes.vue
@@ -1,7 +1,7 @@
 <template>
   <Header class="hidden sm:block" :navigation="navigation" />
-  <div class="md:flex md:items-center md:justify-between mt-10 ml-9 mb-5">
-    <QuizModules />
+  <div v-for="quiz in quizzes" class="md:flex md:items-center md:justify-between ml-9 mb-5">
+    <QuizModules :quiz="quiz" />
   </div>
   <MobileNav :navigation="navigation" />
 </template>
@@ -36,50 +36,97 @@ const navigation = [
   { name: 'Quizzes', href: '/quizzes', current: true, icon: icons.quizzes },
 ]
 
-const quizzes = [{
-  name: 'Module 1',
-  content: [{
-    question: 'Choose the best definition for the term “People Skills” that your group discussed during Module 1 of Good Dads 2.0.',
-    choices: [
-      {
-        content: "Knowing a large number of people and having lots of friends",
-        isCorrect: false
+const quizzes = [
+    {
+      name: 'Module 1',
+      content: [{
+        question: 'Choose the best definition for the term “People Skills” that your group discussed during Module 1 of Good Dads 2.0.',
+        choices: [
+          {
+            content: "Knowing a large number of people and having lots of friends",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the personalities, interests, desires, strengths and weaknesses of those around you.",
+            isCorrect: true
+          }, 
+          {
+            content: "Knowing who likes you and who doesn’t like you.",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the right thing to say all the time.",
+            isCorrect: false
+          }
+        ]
+      }, {
+        question: "Which of the following best describes the traits of a good leader?",
+        choices: [
+          {
+            content: "Sticking to the plan.",
+            isCorrect: false
+          }, 
+          {
+            content: "Commitment",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing which tool to use for a given job.",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the right thing to say all the time.",
+            isCorrect: true
+          }
+        ]
+      }]
+    },
+    {
+      name: 'Module 1',
+      content: [{
+        question: 'Choose the best definition for the term “People Skills” that your group discussed during Module 1 of Good Dads 2.0.',
+        choices: [
+          {
+            content: "Knowing a large number of people and having lots of friends",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the personalities, interests, desires, strengths and weaknesses of those around you.",
+            isCorrect: true
+          }, 
+          {
+            content: "Knowing who likes you and who doesn’t like you.",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the right thing to say all the time.",
+            isCorrect: false
+          }
+        ]
       }, 
       {
-        content: "Knowing the personalities, interests, desires, strengths and weaknesses of those around you.",
-        isCorrect: true
-      }, 
-      {
-        content: "Knowing who likes you and who doesn’t like you.",
-        isCorrect: false
-      }, 
-      {
-        content: "Knowing the right thing to say all the time.",
-        isCorrect: false
-      }
-    ]
-  }, {
-    question: "Which of the following best describes the traits of a good leader?",
-    choices: [
-      {
-        content: "Sticking to the plan.",
-        isCorrect: false
-      }, 
-      {
-        content: "Commitment",
-        isCorrect: false
-      }, 
-      {
-        content: "Knowing which tool to use for a given job.",
-        isCorrect: false
-      }, 
-      {
-        content: "Knowing the right thing to say all the time.",
-        isCorrect: true
-      }
-    ]
-  }]
-},]
+        question: "Which of the following best describes the traits of a good leader?",
+        choices: [
+          {
+            content: "Sticking to the plan.",
+            isCorrect: false
+          }, 
+          {
+            content: "Commitment",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing which tool to use for a given job.",
+            isCorrect: false
+          }, 
+          {
+            content: "Knowing the right thing to say all the time.",
+            isCorrect: true
+          }
+        ]
+      }]
+    },
+]
 
 const { data } = await useAsyncQuery(GET_SCHEDULE, {
   filters: {
